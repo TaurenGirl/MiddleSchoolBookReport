72aad3151fb77a2d0b4fd1d77eb7c308778156a3
Tomi Skibinski <tomi.skibinski@gmail.com>
2023-02-06 08:56:52 -0500

    Changed to Java Project

---
+++ MSBR.java
@@ -0,0 +1,18 @@
+import javax.swing.*;
+
+public class MSBR {
+    
+    JFrame frame;
+    
+    public MSBR() {
+        frame = new JFrame();
+        frame.setSize(400, 500);
+        frame.setLayout(null);
+        frame.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new MSBR();
+    }
+}
+
